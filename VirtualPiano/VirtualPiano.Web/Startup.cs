00142f3ed7e32511c2ecf5e3e3787617a0575368
IvaylaH <ivayla_h@yahoo.com>
2014-11-05 13:06:47 -0800

    Initial commit

---
+++ VirtualPiano/VirtualPiano.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VirtualPiano.Web.Startup))]
+namespace VirtualPiano.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
