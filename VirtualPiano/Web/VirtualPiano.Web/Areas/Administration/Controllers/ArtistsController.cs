c505b16ee0503e9205ae407bb57d00dc4a4d5516
IvaylaH <ivayla_h@yahoo.com>
2014-11-17 18:56:32 -0800

    Added Administration for Music artists.

---
+++ VirtualPiano/Web/VirtualPiano.Web/Areas/Administration/Controllers/ArtistsController.cs
@@ -0,0 +1,80 @@
+﻿namespace VirtualPiano.Web.Areas.Administration.Controllers
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+
+    using Microsoft.AspNet.Identity;
+
+    using VirtualPiano.Data;
+    using VirtualPiano.Models;
+    using VirtualPiano.Web.Areas.Administration.Controllers.Base;
+    using VirtualPiano.Web.Areas.Administration.ViewModels.Artists;
+
+    public class ArtistsController : AdminController
+    {
+        public ArtistsController(IVirtualPianoData data)
+            : base(data)
+        {
+        }
+
+        // GET: Administration/Artists
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Read([DataSourceRequest]DataSourceRequest request)
+        {
+            var musicSheets = this.Data.Artists.All()
+                .Project()
+                .To<ArtistsViewModel>()
+                .ToDataSourceResult(request);
+
+            return Json(musicSheets);
+        }
+
+        [HttpPost]
+        public ActionResult Create([DataSourceRequest]DataSourceRequest request, ArtistsViewModel inputModel)
+        {
+            if (inputModel != null && ModelState.IsValid)
+            {
+                var dbModel = new Artist()
+                {
+                    Name = inputModel.Name,
+                    CreatedOn = DateTime.Now
+                };
+
+                this.Data.Artists.Add(dbModel);
+                this.Data.SaveChanges();
+
+                inputModel.Id = dbModel.Id;
+            }
+
+            return Json(new[] { inputModel }.ToDataSourceResult(request, ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult Update([DataSourceRequest]DataSourceRequest request, ArtistsViewModel inputModel)
+        {
+            if (inputModel != null && ModelState.IsValid)
+            {
+                var dbModel = this.Data.Artists.GetById(inputModel.Id);
+
+                dbModel.Name = inputModel.Name;
+                dbModel.ModifiedOn = DateTime.Now;
+
+                this.Data.SaveChanges();
+            }
+
+            return Json(new[] { inputModel }.ToDataSourceResult(request, ModelState));
+        }
+    }
+}
---
+++ VirtualPiano/Web/VirtualPiano.Web/Areas/Administration/ViewModels/Artists/ArtistsViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace VirtualPiano.Web.Areas.Administration.ViewModels.Artists
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    using VirtualPiano.Models;
+    using VirtualPiano.Web.Infrastructure.Mapping;
+
+    public class ArtistsViewModel : IMapFrom<Artist>
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Display(Name = "Artist/Composer Name")]
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
